8dfae3ce4f09ef3e7d74e8460dc08ab64302986f
Balazs Kreith <kreith.balazs@gmail.com>
2020-12-21 15:00:36 +0200

    change the update script

--- update-helm-repo.sh
+++ update-helm-repo.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set +x
 
-#if git diff-index --quiet HEAD --; then
-#    echo "You have committed every changes necessary to go forward. Good! Just to be sure git push is being called"
-#    git push
-#else
-#    echo "There are changes you have not committed, and therefore you cannot make packages right now."
-#    echo "Please commit your changes before you try to update the helm repo"
-#    exit 1
-#fi
+if git diff-index --quiet HEAD --; then
+    echo "You have committed every changes necessary to go forward. Good! Just to be sure git push is being called"
+    git push
+else
+    echo "There are changes you have not committed, and therefore you cannot make packages right now."
+    echo "Please commit your changes before you try to update the helm repo"
+    exit 1
+fi
 
 for dir in */ ; do
     helm package $dir
